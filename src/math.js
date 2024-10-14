7db6e77377ffd4163c51cfa49717c91955dc37cc
raunerlucas <raunerlucas@gmail.com>
2024-10-14 16:21:30 -0300

    fix: TODOs da atv. resolvida

--- src/math.js
+++ src/math.js
@@ -8,7 +8,9 @@
  * @param {Array} numeros vetor com os números desejados
  */
 function somaDobroNumerosParesFuncional(numeros){
-    // TODO: Insira seu código funcional aqui
+    return numeros.filter(value => value % 2 === 0)
+        .map(value => value * 2)
+        .reduce((acc, value) => acc + value, 0);
 }
 
 /**
@@ -17,7 +19,13 @@ function somaDobroNumerosParesFuncional(numeros){
  * @param {Array} numeros vetor com os números desejados
  */
 function somaDobroNumerosParesProcedural(numeros){
-    // TODO: Insira seu código estruturado aqui   
+    let somaQuadPares = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 === 0) {
+            somaQuadPares += numeros[i] * 2;
+        }
+    }
+    return somaQuadPares;
 }
 
 export { somaDobroNumerosParesFuncional, somaDobroNumerosParesProcedural };
